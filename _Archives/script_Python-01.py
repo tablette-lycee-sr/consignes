7f18cae80cf4f6be81f457cbb1ada23139eb4ad0
tablette-lycee-sr <85511844+tablette-lycee-sr@users.noreply.github.com>
2024-09-18 20:13:18 +0000

    script modifié

---
+++ _Archives/script_Python-01.py
@@ -0,0 +1,127 @@
+"""
+╔══════════════════════════════════════╗
+║       PROGRAMMER AVEC PYTHON         ║
+║               Activité               ║
+║             PREMIERS PAS             ║
+║              (Partie 1)              ║
+╚══════════════════════════════════════╝
+"""
+#|
+"""
+ÉTAPE 01. 
+Découvrir comment fonctionne un script Python
+"""
+#|____________________________________________
+#|   01-1.  Les instructions
+#| A.
+print("Bienvenue !")
+
+#|____________________________________________
+#|   01-2.  La syntaxe
+#| A.
+#    Print("Ça marche !!!")
+
+"""
+ÉTAPE 02.
+Afficher des données dans la console avec la fonction print()
+"""
+#|____________________________________________
+#|   02-1.  Les données numériques
+#| A.
+print(10)
+#| B.
+print([nbJours]-[dateJour ))
+#| C.
+print(36*1.5)
+
+#|____________________________________________
+#|   02-2.  Les chaînes de caractères
+#| A.
+print("Salut")
+#| B.
+print("Je m'appelle "+ "Bob")
+#| C.
+print(500 * "O X ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a="Bienvenue dans Python !"
+print()
+import _verif03_1A
+try:
+  _verif03_1A.verifA(score)
+except:
+  _verif03_1A.erreurA()
+print()  
+import _verif03_1B
+try:
+  _verif03_1B.verifB(invisible)
+except:
+  _verif03_1B.erreurB()
+print()
+import _verif03_1C
+try:
+  _verif03_1C.verifC(nomDuJoueur)
+except:
+  _verif03_1C.erreurC()
+print()

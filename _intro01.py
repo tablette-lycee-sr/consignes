7abe1e6be21b7d8043f379bfd496bbcfac87098b
tablette-lycee-sr <85511844+tablette-lycee-sr@users.noreply.github.com>
2024-09-23 21:21:01 +0000

    P5 5

--- _intro01.py
+++ _intro01.py
@@ -1,11 +1,13 @@
 from p5 import *
+from random import randint
 
 lettres = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','W','X','Y','Z']
-lettre1 = int(random(0, 24))
-lettre2 = int(random(0, 24))
-lettre3 = int(random(0, 24))
-numerohasard = int(random(1, 9))
-code = str(numerohasard)+str(lettres[lettre1])+str(lettres[lettre2])+str(lettres[lettre3])+str(10-numerohasard)
+lettre1 = int(randint(0, 24))
+lettre2 = int(randint(0, 24))
+lettre3 = int(randint(0, 24))
+numerohasard = int(randint(1, 9))
+global code
+code = str(numerohasard)+str(lettres[lettre1])+str(lettres[lettre2])+str(lettres[lettre3])+"X"+str(10-numerohasard)
 
 def setup():
     createCanvas(400, 400)
@@ -20,14 +22,18 @@ def draw():
    
     textSize(18)
     fill('tomato')
-    text("Python c'est aussi du dessin !", 50, 300)
+    text("Python c'est aussi du dessin !", 90, 30)
     fill('cornflowerblue')
+    text("Notez le code ci-dessous sur votre cahier", 20, 270)
+    text("et revenez dans la console ( bouton >_ )", 20, 290)
+    text("Saissez ce code au prompt >>> ", 20, 310)
+    text("puis appuyez sur Entrée", 20, 330)
     textSize(30)
-    text(code, 100, 340)
+    text(code, 150, 360)
 
     # Draw left eye
     leftX = 150
-    leftY = 200
+    leftY = 150
     # Calculate angle between left eye and mouse
     leftAngle = atan2(mouseY - leftY, mouseX - leftX)
     push()
@@ -40,7 +46,7 @@ def draw():
     pop()
     # Draw right eye
     rightX = 250
-    rightY = 200
+    rightY = 150
     # Calculate angle between right eye and angle
     rightAngle = atan2(mouseY - rightY, mouseX - rightX)
     push()

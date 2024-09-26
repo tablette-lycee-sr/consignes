2b98c108c4bdb3293729487a7c613ea8cfc4895a
tablette-lycee-sr <85511844+tablette-lycee-sr@users.noreply.github.com>
2024-09-26 17:32:46 +0000

    Fichier python de validation

---
+++ assets/validation_th0-1.py
@@ -0,0 +1,13 @@
+
+
+from p5 import *
+
+
+import _intro01
+
+run(setup=_intro01.setup, draw=_intro01.draw)
+
+codeeleve=str(input(">>>"))
+print("Merci ! Votre code "+codeeleve+" a bien été enregistré !")
+
+
